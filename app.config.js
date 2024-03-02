2bd617420ad95f0a88eddbc6ad7ad7ccdddbfe72
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2024-03-01 21:36:51 -0400

    solucionando problemas con variables de entorno

--- app.config.js
+++ app.config.js
@@ -2,40 +2,45 @@ import "dotenv/config";
 
 export default{
   expo: {
-    name: "YourAppName",
-    slug: "your-app-slug",
+    name: "avi-pro-mobile",
+    slug: "avi-pro-mobile",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
+      githubUrl: ""
     },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ["**/*"],
+    assetBundlePatterns: [
+      "**/*"
+    ],
     ios: {
-      supportsTablet: true,
+      supportsTablet: true
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff",
+        backgroundColor: "#ffffff"
       },
+      package: "com.endurancesoft.avipromobile"
     },
     web: {
-      favicon: "./assets/favicon.png",
+      favicon: "./assets/favicon.png"
     },
     extra: {
-        apiKey: process.env.API_KEY,
-        authDomain: process.env.AUTH_DOMAIN,
-        projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
-        messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID,
-        measurementId: process.env.MEASUREMENT_ID,
+      eas: {
+        "projectId": "68722059-896d-4f90-9039-57984284a8d6"
+      },
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
       }
   },
 };
