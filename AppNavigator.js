af169540201481f7f5db6f38cbaa2458be6857e4
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-07 19:33:45 -0400

    feat(invoice): BillScreen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,37 +1,64 @@
 import React from 'react';
-import { Easing } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import NewScreen from './src/screens/HomeScreen';
 import ClientSearchScreen from './src/screens/ClientSearchScreen';
 import BillScreen from './src/screens/BillScreen'
-
+import { Animated, Easing } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 function AppNavigator() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="NewScreen" > */}
-      <Stack.Navigator initialRouteName="BillScreen" >
+      <Stack.Navigator
+      screenOptions={{
+        cardStyleInterpolator: ({current, layouts}) => {
+          return {
+            cardStyle: {
+              opacity: current.progress,
+              transform: [
+                {
+                  translateY: current.progress.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [layouts.screen.height * 0.3, 0],
+                  }),
+                },
+              ],
+            },
+          };
+        },
+        transitionSpec: {
+          open: {
+            animation: 'timing',
+            config: {
+              duration: 150,
+              easing: Easing.ease,
+            },
+          },
+          close: {
+            animation: 'timing',
+            config: {
+              duration: 150,
+              easing: Easing.ease,
+            },
+          },
+        },
+      }}
+      >
         <Stack.Screen 
-            name="NewScreen" 
-            component={NewScreen} 
-            options={{ 
-                headerShown: false,
-            }} />
-        <Stack.Screen 
-            name="ClientSearchScreen" 
-            component={ClientSearchScreen} 
-            options={{
-                headerShown: false,
-            }} 
+          name="NewScreen" 
+          component={NewScreen} 
+          options={{ headerShown: false }} 
         />
         <Stack.Screen 
-            name="BillScreen" 
-            component={BillScreen} 
-            options={{
-                headerShown: false,
-            }} 
+          name="ClientSearchScreen" 
+          component={ClientSearchScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="BillScreen" 
+          component={BillScreen} 
+          options={{ headerShown: false }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
