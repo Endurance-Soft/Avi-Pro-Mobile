0ac35173710c41a9de0156fedfdf628e66a25c45
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-09 19:46:46 -0400

    Update ClientItem.js

--- src/components/ClientItem.js
+++ src/components/ClientItem.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { styles } from "./styles/ClientItemStyles";
+import { theme } from '../../constants';
+const windowWidth = Dimensions.get('window').width;
 
 const ClientItem = ({ client, onSelect }) => {
   return (
@@ -38,4 +39,78 @@ const ClientItem = ({ client, onSelect }) => {
   );
 };
 
+const styles = StyleSheet.create({
+  item: { 
+    paddingVertical: 13,
+    paddingHorizontal: 17,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: theme.colors.otherWhite,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconWraped: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.tertiary,
+    borderRadius: 20,
+    width: 70,
+    height: 70,
+  },
+  icon: {
+    color: theme.colors.primary,
+    fontSize: 30,
+    fontWeight: 'medium',
+  },
+  detailsContainer: {
+    marginLeft: 17,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: theme.colors.tertiary
+  },
+  codeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  code: {
+    fontSize: 16,
+    marginLeft: 5,
+    color: theme.colors.secondaryText,
+  },
+  balance: {
+    fontSize: 16,
+    marginLeft: 8,
+    color: theme.colors.secondaryText,
+  },
+  notesContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  notes: {
+    fontSize: 16,
+    color: theme.colors.secondaryText,
+  },
+  lastPayment: {
+    fontSize: 16,
+    color: theme.colors.secondaryText,
+  },
+  lineContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  line: {
+    marginVertical: 13 ,
+    backgroundColor: theme.colors.otherWhite,
+    width: windowWidth*0.8,
+    height: 2,
+  },
+});
 export default ClientItem;
