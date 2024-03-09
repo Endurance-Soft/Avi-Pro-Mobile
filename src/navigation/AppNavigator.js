062289f274f0a78fbfa3a0cea1d1652d07425cb8
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-09 19:59:24 -0400

    Update App

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import NewScreen from './src/screens/HomeScreen';
-import ClientSearchScreen from './src/screens/ClientSearchScreen';
-import BillScreen from './src/screens/BillScreen'
-import ClientPaymentScreen from './src/screens/ClientPaymentScreen';
+import NewScreen from '../screens/HomeScreen';
+import ClientSearchScreen from '../screens/ClientSearchScreen';
+import BillScreen from '../screens/BillScreen'
+import ClientPaymentScreen from '../screens/ClientPaymentScreen';
 
 const Stack = createNativeStackNavigator();
 
