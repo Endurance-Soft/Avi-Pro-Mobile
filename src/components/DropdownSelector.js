8c5a0b666d7de5bfed61bb1bcf15785722a1b50e
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-09 19:36:43 -0400

    Update DropdownSelector.js

--- src/components/DropdownSelector.js
+++ src/components/DropdownSelector.js
@@ -3,7 +3,9 @@ import React, { useState, useCallback } from "react";
 import { View, Text } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from "react-native-popup-menu";
-import {styles} from './styles/DropdownSelectorStyles'
+import { theme } from '../../constants';
+import { StyleSheet, Dimensions } from 'react-native';
+const screenWidth = Dimensions.get('window').width;
 
 const DropdownSelector = ({ title, options, selectedOption, onOptionChange }) => {
   const [menuVisible, setMenuVisible] = useState(false);
@@ -45,4 +47,63 @@ const DropdownSelector = ({ title, options, selectedOption, onOptionChange }) =>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    borderRadius: 20,
+    backgroundColor: theme.colors.skyBlue,
+    padding: 7,
+    flexDirection: "row",
+    justifyContent: 'flex-end',
+  },
+  label: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'center',
+    borderRadius: 20,
+    flex:1
+  },
+  optionText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  optionsText: {
+    color: theme.colors.primary,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  menuTrigger: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    backgroundColor: theme.colors.tertiary,
+    borderRadius: 22,
+    width: 200,
+  },
+  menuTriggerInter: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  triggerText: {
+    color: theme.colors.primary,
+      fontSize: 16,
+      fontWeight: "bold",
+      marginRight: 12,
+  },
+  optionsContainer: {
+    paddingVertical: 15,
+    marginTop: 55,
+    marginLeft: 0,
+    borderRadius: 20,
+    width: 200,
+    backgroundColor: theme.colors.tertiary,
+  },
+  optionsWrapper: {
+    marginLeft: 20,
+  },
+});
 export default DropdownSelector;
