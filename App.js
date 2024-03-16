3ad25fa51da930cc63b14d0ba26bef4bec1e9344
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-16 10:27:52 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { View } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
 import AppNavigator from "./src/navigation/AppNavigator";
-import StyledText from "./src/utils/StyledText";
 
 const App = () => {
   return (
     <MenuProvider>
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <View style={{ alignItems: "center", marginTop: 20, gap:20 }}>
-          <StyledText regularText>Texto Regular</StyledText>
-          <StyledText boldText>Texto en Negrita</StyledText>
-          <View style={{ backgroundColor: 'gray'}}>
-            <StyledText buttonText>Texto de Botón</StyledText>
-          </View>
-        </View>
-      </View>
+      <AppNavigator/>
     </MenuProvider>
   );
 };
