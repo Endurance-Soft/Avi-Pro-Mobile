6f14f5d46b53f91a324e5b3b8044b5b90342a708
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-16 10:12:33 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text, View, Dimensions } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
 import AppNavigator from "./src/navigation/AppNavigator";
+import BorderBox from "./src/utils/BorderBox";
+
+const width = Dimensions.get("window").width;
 
 const App = () => {
+  const [pressCount, setPressCount] = useState(0);
   return (
     <MenuProvider>
-      <AppNavigator />
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <BorderBox onPress={() => setPressCount(pressCount + 1)} style={{ alignSelf: 'center', marginTop: 50 }}>
+          <Text>{`Presionado ${pressCount} veces`}</Text>
+        </BorderBox>
+        <BorderBox onPress={() => setPressCount(pressCount + 1)} style={{ alignSelf: 'center', marginTop: 50, width: width-40 }}>
+          <Text>Contenido</Text>
+        </BorderBox>
+      </View>
+      {/* <AppNavigator /> */}
     </MenuProvider>
   );
 };
