e4d1a3b2ba129f71201657643bfa3f5aedaf987d
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-03-16 10:14:35 -0400

    Update SimpleButton.js

--- src/utils/SimpleButton.js
+++ src/utils/SimpleButton.js
@@ -8,7 +8,7 @@ const SimpleButton = ({ text, onPress, width }) => {
 
   const animatePressIn = () => {
     Animated.timing(scale, {
-      toValue: 0.85,
+      toValue: 0.90,
       duration: 100,
       useNativeDriver: true,
     }).start();
@@ -23,10 +23,10 @@ const SimpleButton = ({ text, onPress, width }) => {
   };
 
   const handlePress = () => {
-    animatePressIn();
-    setTimeout(() => {
-      animatePressOut();
-    }, 200);
+    // animatePressIn();
+    // setTimeout(() => {
+    //   animatePressOut();
+    // }, 200);
     onPress();
   };
 
