bc4cb98de6b27d9830fe12644db678cf44500bec
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-04-07 20:36:37 -0400

    Update ClientDebit.js

--- src/components/ClientDebit.js
+++ src/components/ClientDebit.js
@@ -8,8 +8,6 @@ import {
   StyleSheet,
 } from "react-native";
 import { theme } from "../../constants";
-import { useFocusEffect } from "@react-navigation/native";
-import Modal from "../modals/SimpleModal";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
 import SimpleButton from "../utils/SimpleButton";
@@ -24,50 +22,12 @@ const ClientDebit = ({ clientInfo }) => {
       0
     ).toFixed(2)
   );
-  const [totalDebit, setTotalDebit] = useState(0);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [animationKey, setAnimationKey] = useState(Date.now());
-  const [statusBarColor, setStatusBarColor] = useState("#84A1A7");
-  const colorSteps = [
-    "#B9E4EA",
-    "#AFD7DC",
-    "#9DC1C6",
-    "#89A8AC",
-    "#7A969A",
-    "#6D8689",
-    "#647B7E",
-    "#5F7477",
-  ];
-  useEffect(() => {
-    let timers = [];
-    if (modalVisible) {
-      const interval = 30;
-      colorSteps.forEach((color, index) => {
-        let timer = setTimeout(() => {
-          setStatusBarColor(color);
-        }, interval * index);
-        timers.push(timer);
-      });
-    } else {
-      setStatusBarColor(theme.colors.secondary);
-    }
-    return () => timers.forEach((timer) => clearTimeout(timer));
-  }, [modalVisible, theme.colors.secondary]);
-  useFocusEffect(
-    useCallback(() => {
-      setAnimationKey(Date.now());
-    }, [])
-  );
-  const toggleModal = () => {
-    setModalVisible(!modalVisible);
-  };
 
   const navigation = useNavigation();
 
   return (
     <View style={clientDebitStyles.container}>
-      <StatusBar style="ligth" backgroundColor={statusBarColor} />
-      <Modal isVisible={modalVisible} onClose={toggleModal} />
+      <StatusBar style="ligth" backgroundColor={theme.colors.secondary} />
       <StyledText balance style={clientDebitStyles.text}> {vBalance} Bs</StyledText>
       <View style={clientDebitStyles.spaceButtons}>
         <SimpleButton
