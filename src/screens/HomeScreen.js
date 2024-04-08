a2a3dc00599cb3600becd8d6c3b0cf3c0f731de6
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-04-07 20:43:16 -0400

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import React, { useState, useCallback } from "react";
 import { SafeAreaView, StyleSheet, View, FlatList, Text } from "react-native";
 import ProfileHeader from "../components/ProfileHeader";
 import StoryItem from "../components/StoryItem";
-import { HISTORY_DATA, theme } from "../../constants";
+import { theme } from "../../constants";
 import DropdownSelector from "../components/DropdownSelector";
 import Cascading from "../animation/CascadingFadeInView";
 import { useFocusEffect } from "@react-navigation/native";
@@ -13,6 +13,87 @@ const NewScreen = () => {
   const OPCIONES = ['Hoy', 'Esta Semana', 'Este Mes', 'Todo'];
   const title = 'Actividad';
 
+  const HISTORY_DATA = [
+    {
+      id: "1",
+      name: "Samuel Herbas",
+      amount: "130.00",
+      date: "2024-03-01T12:32:00",
+      note: "150",
+    },
+    { id: "2", 
+      name: "Henry Peña", 
+      amount: "70.00", 
+      date: "2024-03-01T09:16:00", 
+      note: "170" },
+    {
+      id: "3",
+      name: "Louisa Cespedez",
+      amount: "200.00",
+      date: "2024-03-01T06:30:00",
+      note: "171"
+    },
+    {
+      id: "4",
+      name: "Samuel Herbas",
+      amount: "130.00",
+      date: "2024-03-01T12:32:00",
+      note: "151",
+    },
+    { id: "5", 
+      name: "Henry Peña",
+      amount: "70.00", 
+      date: "2024-03-01T09:16:00",
+      note: "161" },
+    {
+      id: "6",
+      name: "Louisa Cespedez",
+      amount: "200.00",
+      date: "2024-03-01T06:30:00",
+      note: "172",
+    },
+    {
+      id: "7",
+      name: "Samuel Herbas",
+      amount: "130.00",
+      date: "2024-03-01T12:32:00",
+      note: "150"
+    },
+    { id: "8", 
+      name: "Henry Peña", 
+      amount: "70.00", 
+      date: "2024-03-01T09:16:00", 
+      note:"121" },
+    {
+      id: "9",
+      name: "Louisa Cespedez",
+      amount: "200.00",
+      date: "2024-03-01T06:30:00",
+      note: "161",
+    },
+    {
+      id: "10",
+      name: "Samuel Herbas",
+      amount: "130.00",
+      date: "2024-03-01T12:32:00",
+      note: "150",
+    },
+    {
+      id: "11",
+      name: "Henry Peña",
+      amount: "70.00",
+      date: "2024-03-01T09:16:00",
+      note: "180",
+    },
+    {
+      id: "12",
+      name: "Louisa Cespedez",
+      amount: "200.00",
+      date: "2024-03-01T06:30:00",
+      note: "190",
+    },
+  ];
+  
   const renderHistoryItem = ({ item, index }) => (
     <Cascading delay={400 + 80 * index} animationKey={animationKey}>
       <StoryItem
